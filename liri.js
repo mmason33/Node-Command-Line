97f849b5580b5fc0703db618a359403a02880ad6
Michael Mason <michael@michaeljmason>
2017-08-17 18:00:32 -0700

    recreated problem

--- liri.js
+++ liri.js
@@ -1,40 +1,48 @@
 //https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
 
 var keys = require('./keys.js');
-var twitter = require("twitter");
+// var twitter = require("twitter");
 // var request = require("request");
 
 // console.log(api);
+// 9951bddf3af7e21abdb61ad50b4325a2
+// api.openweathermap.org/data/2.5/forecast?id=524901&APPID=9951bddf3af7e21abdb61ad50b4325a2
+var api = new XMLHttpRequest();
 
+api.onreadystatechange = function() {
+  console.log(api.responseText);
+}
+api.open('GET', 'api.openweathermap.org/data/2.5/forecast?id=524901&APPID=9951bddf3af7e21abdb61ad50b4325a2' );
+api.send();
 
-  var client = new twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key: keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret,
-  });
-  var twitterUsername = process.argv[3];
-  if(!twitterUsername){
-    twitterUsername = "FirstTake";
-  }
-  params = {screen_name: twitterUsername};
-  client.get("statuses/user_timeline/", params, function(error, data, response){
-
-    // console.log(response);
-
-    if (!error) {
-      for(var i = 0; i < data.length; i++) {
-        //console.log(response); // Show the full response in the terminal
-        var twitterResults =
-        "@" + data[i].user.screen_name + ": " +
-        data[i].text + "\r\n" +
-        data[i].created_at + "\r\n" +
-        "------------------------------ " + i + " ------------------------------" + "\r\n";
-        console.log(twitterResults);
-        //log(twitterResults); // calling log function
-      }
-    }  else {
-      console.log("Error :"+ error);
-      return;
-    }
-  });
+  // var client = new twitter({
+  //   consumer_key: keys.twitterKeys.consumer_key,
+  //   consumer_secret: keys.twitterKeys.consumer_secret,
+  //   access_token_key: keys.twitterKeys.access_token_key,
+  //   access_token_secret: keys.twitterKeys.access_token_secret,
+  // });
+  // var twitterUsername = process.argv[3];
+  // if(!twitterUsername){
+  //   twitterUsername = "FirstTake";
+  // }
+  // params = {screen_name: twitterUsername};
+  // client.get("statuses/user_timeline/", params, function(error, data, response){
+  //
+  //   // console.log(response);
+  //
+  //   if (!error) {
+  //     for(var i = 0; i < data.length; i++) {
+  //       //console.log(response); // Show the full response in the terminal
+  //       var twitterResults =
+  //       "@" + data[i].user.screen_name + ": " +
+  //       data[i].text + "\r\n" +
+  //       data[i].created_at + "\r\n" +
+  //       "------------------------------ " + i + " ------------------------------" + "\r\n";
+  //       console.log(twitterResults);
+  //       //log(twitterResults); // calling log function
+  //     }
+  //   }  else {
+  //     console.log("Error :"+ error);
+  //     return;
+  //   }
+  // });
