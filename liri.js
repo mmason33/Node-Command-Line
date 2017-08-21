b53c504bf1943f5695db34459f4ec58984c33287
michael mason <michael@michaeljmason.com>
2017-08-21 11:43:01 -0700

    finished

--- liri.js
+++ liri.js
@@ -1,36 +1,78 @@
-//https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
-
-var keys = require('./keys.js');
-var twitter = require('twitter');
-var Spotify = require('node-spotify-api');
-var request = require('request');
-var movies = [];
-//creat logic for the process.argv[index] params when calling these function in command line
-// create a switch statement for the process.argv[index]
-
-switch(process.argv[2]) {
-  case 'twitter':
-    tweets(process.argv[3]);
+// Node module imports needed to run the functions
+var fs = require("fs"); //reads and writes files
+var request = require("request");
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var Spotify = require ("node-spotify-api");
+var liriArgument = process.argv[2];
+// ---------------------------------------------------------------------------------------------------------------
+// Possible commands for this liri app
+switch(liriArgument) {
+  case "my-tweets":
+    myTweets();
     break;
-  case 'spotify':
-    spotify(process.argv[3]);
+  case "spotify-this-song":
+    spotifyThisSong();
     break;
-  case 'omdb':
-    omdb(process.argv);
+  case "movie-this":
+    movieThis();
     break;
-  default:
-    console.log('Not a valid command');
-}
-
-function tweets(arg) {
-    var client = new twitter({
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  // Instructions displayed in terminal to the user
+  default: console.log("\r\n" +"Try typing one of the following commands after 'node liri.js' : " +"\r\n"+
+    "1. my-tweets 'any twitter name' " +"\r\n"+
+    "2. spotify-this-song 'any song name' "+"\r\n"+
+    "3. movie-this 'any movie name' "+"\r\n"+
+    "4. do-what-it-says."+"\r\n"+
+    "Be sure to put the movie or song name in quotation marks if it's more than one word.");
+};
+// ---------------------------------------------------------------------------------------------------------------
+// Functions
+// Movie function, uses the Request module to call the OMDB api
+function movieThis(){
+  var movie = process.argv;
+  if(!movie){
+    movie = "Inception";
+  } else {
+    var params = '';
+    for (var i = 3;i < movie.length; i++) {
+        params = params + movie[i] + '+';
+    }
+  }
+  request("http://www.omdbapi.com/?apikey=" + keys.omdbKey + "&t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var movieObject = JSON.parse(body);
+      var movieResults =
+      "------------------------------ begin ------------------------------" + "\r\n" +
+      "Title: " + movieObject.Title+"\r\n"+
+      "Year: " + movieObject.Year+"\r\n"+
+      "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+      "Country: " + movieObject.Country+"\r\n"+
+      "Language: " + movieObject.Language+"\r\n"+
+      "Plot: " + movieObject.Plot+"\r\n"+
+      "Actors: " + movieObject.Actors+"\r\n"+
+      "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+      "Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" +
+      "------------------------------ end ------------------------------" + "\r\n";
+      console.log(movieResults);
+      log(movieResults); // calling log function
+    } else {
+      console.log("Error :"+ error);
+      return;
+    }
+  });
+};
+// Tweet function, uses the Twitter module to call the Twitter api
+function myTweets() {
+  var client = new twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
     access_token_key: keys.twitterKeys.access_token_key,
     access_token_secret: keys.twitterKeys.access_token_secret,
   });
-
-  var twitterUsername = arg;
+  var twitterUsername = process.argv[3];
   if(!twitterUsername){
     twitterUsername = "FirstTake";
   }
@@ -38,14 +80,14 @@ function tweets(arg) {
   client.get("statuses/user_timeline/", params, function(error, data, response){
     if (!error) {
       for(var i = 0; i < data.length; i++) {
-        //console.log(response);
+        //console.log(response); // Show the full response in the terminal
         var twitterResults =
         "@" + data[i].user.screen_name + ": " +
         data[i].text + "\r\n" +
         data[i].created_at + "\r\n" +
         "------------------------------ " + i + " ------------------------------" + "\r\n";
         console.log(twitterResults);
-        //log(twitterResults);
+        log(twitterResults); // calling log function
       }
     }  else {
       console.log("Error :"+ error);
@@ -53,19 +95,24 @@ function tweets(arg) {
     }
   });
 }
-
-function spotify(arg) {
+// Spotify function, uses the Spotify module to call the Spotify api
+function spotifyThisSong(songName) {
 
   var spotify = new Spotify({
     id: keys.spotifyKey.client_id,
     secret: keys.spotifyKey.client_secret,
   });
 
-  var defaultSong = 'Straight Outta Compton';
-  if (arg !== undefined) {
-    defaultSong = arg;
+  var songName = process.argv;
+  if(!songName){
+    songName = "Straight Outta Compton";
+  } else {
+    var params = '';
+    for (var i = 3;i < songName.length; i++) {
+        params = params + songName[i] + '+';
+    }
   }
-  spotify.search({ type: 'track', query: defaultSong }, function(err, data) {
+  spotify.search({ type: "track", query: params }, function(err, data) {
     if(!err){
       var songInfo = data.tracks.items;
       for (var i = 0; i < 5; i++) {
@@ -77,42 +124,31 @@ function spotify(arg) {
           "Preview Url: " + songInfo[i].preview_url + "\r\n" +
           "------------------------------ " + i + " ------------------------------" + "\r\n";
           console.log(spotifyResults);
+          log(spotifyResults); // calling log function
         }
       }
     }	else {
       console.log("Error :"+ err);
       return;
     }
-
   });
-
-}
-
-function omdb(arg) {
-  var url = 'http://www.omdbapi.com/?apikey=';
-  var defaultTitle = 'Inception';
-  var newTitle = '';
-  if (arg !== undefined) {
-    for (var i = 3; i < arg.length; i++) {
-      newTitle = newTitle + arg[i] + '+';
+};
+// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data){
+    if (!error) {
+      doWhatItSaysResults = data.split(",");
+      spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+    } else {
+      console.log("Error occurred" + error);
     }
-  }
-  defaultTitle = newTitle;
-  console.log(defaultTitle);
-  request(url + keys.omdbKey + '&t=' + defaultTitle, function (error, response, body) {
-    var obj = JSON.parse(body);
-    var res =
-    "------------------------------ begin ------------------------------" + "\r\n" +
-    "Title: " + obj.Title+"\r\n"+
-    "Year: " + obj.Year+"\r\n"+
-    "Imdb Rating: " + obj.imdbRating+"\r\n"+
-    "Country: " + obj.Country+"\r\n"+
-    "Language: " + obj.Language+"\r\n"+
-    "Plot: " + obj.Plot+"\r\n"+
-    "Actors: " + obj.Actors+"\r\n"+
-    "Rotten Tomatoes Rating: " + obj.Ratings[1].Value+"\r\n"
-    "------------------------------ fin ------------------------------" + "\r\n";
-    console.log(res);
   });
-
+};
+// Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
+function log(logResults) {
+  fs.appendFile("log.txt", logResults, (error) => {
+    if(error) {
+      throw error;
+    }
+  });
 }
