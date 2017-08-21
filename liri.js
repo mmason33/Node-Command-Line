b761e015431e4786d917c3102bdd45c2f8a1ce0c
michael mason <michael@michaeljmason.com>
2017-08-21 11:21:26 -0700

    traversed objects

--- liri.js
+++ liri.js
@@ -4,11 +4,25 @@ var keys = require('./keys.js');
 var twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
-
+var movies = [];
 //creat logic for the process.argv[index] params when calling these function in command line
 // create a switch statement for the process.argv[index]
 
-function tweets() {
+switch(process.argv[2]) {
+  case 'twitter':
+    tweets(process.argv[3]);
+    break;
+  case 'spotify':
+    spotify(process.argv[3]);
+    break;
+  case 'omdb':
+    omdb(process.argv);
+    break;
+  default:
+    console.log('Not a valid command');
+}
+
+function tweets(arg) {
     var client = new twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
@@ -16,7 +30,7 @@ function tweets() {
     access_token_secret: keys.twitterKeys.access_token_secret,
   });
 
-  var twitterUsername = process.argv[3];
+  var twitterUsername = arg;
   if(!twitterUsername){
     twitterUsername = "FirstTake";
   }
@@ -40,35 +54,65 @@ function tweets() {
   });
 }
 
-function spotify() {
+function spotify(arg) {
 
   var spotify = new Spotify({
     id: keys.spotifyKey.client_id,
     secret: keys.spotifyKey.client_secret,
   });
 
-  spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+  var defaultSong = 'Straight Outta Compton';
+  if (arg !== undefined) {
+    defaultSong = arg;
+  }
+  spotify.search({ type: 'track', query: defaultSong }, function(err, data) {
+    if(!err){
+      var songInfo = data.tracks.items;
+      for (var i = 0; i < 5; i++) {
+        if (songInfo[i] != undefined) {
+          var spotifyResults =
+          "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+          "Song: " + songInfo[i].name + "\r\n" +
+          "Album the song is from: " + songInfo[i].album.name + "\r\n" +
+          "Preview Url: " + songInfo[i].preview_url + "\r\n" +
+          "------------------------------ " + i + " ------------------------------" + "\r\n";
+          console.log(spotifyResults);
+        }
+      }
+    }	else {
+      console.log("Error :"+ err);
+      return;
     }
 
-  console.log(data);
   });
 
 }
 
-function omdb() {
+function omdb(arg) {
   var url = 'http://www.omdbapi.com/?apikey=';
   var defaultTitle = 'Inception';
-
+  var newTitle = '';
+  if (arg !== undefined) {
+    for (var i = 3; i < arg.length; i++) {
+      newTitle = newTitle + arg[i] + '+';
+    }
+  }
+  defaultTitle = newTitle;
+  console.log(defaultTitle);
   request(url + keys.omdbKey + '&t=' + defaultTitle, function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body);
+    var obj = JSON.parse(body);
+    var res =
+    "------------------------------ begin ------------------------------" + "\r\n" +
+    "Title: " + obj.Title+"\r\n"+
+    "Year: " + obj.Year+"\r\n"+
+    "Imdb Rating: " + obj.imdbRating+"\r\n"+
+    "Country: " + obj.Country+"\r\n"+
+    "Language: " + obj.Language+"\r\n"+
+    "Plot: " + obj.Plot+"\r\n"+
+    "Actors: " + obj.Actors+"\r\n"+
+    "Rotten Tomatoes Rating: " + obj.Ratings[1].Value+"\r\n"
+    "------------------------------ fin ------------------------------" + "\r\n";
+    console.log(res);
   });
 
 }
-
-// omdb();
-// spotify();
-// tweets();
